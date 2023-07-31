fcb0e81eb1077bcb6c572881b44a274c166e6e09
SoumenBaral <shumen.sb@gmail.com>
2023-07-31 16:01:53 +0600

    props and distructing done

--- 1.Mordern frontend core concept/1.rest countries recap/countries.js
+++ 1.Mordern frontend core concept/1.rest countries recap/countries.js
@@ -11,12 +11,36 @@ const displayData = countries => {
     
     container.innerHTML = allCountryHtml.join(' ');
 }
-const getCountryHtml = country=>{
- return `
-    <div class ='country'>
-        <h1>${country.name.common}</h1>
-        <img src="${country.flags.png}">
-    </div>
- `
-}
+const getCountryHtml = ({name,flags,area,region})=>{
+    return `
+       <div class ='country'>
+           <h1>${name.common}</h1>
+           <p>Area: ${area}</p>
+           <p>Region: ${region}</p>
+           <img src="${flags.png}">
+       </div>
+    `
+   }
+// Option 1;
+// const getCountryHtml = country=>{
+//     //Option  1;
+//     const {name, flags} = country;
+//  return `
+//     <div class ='country'>
+//         <h1>${name.common}</h1>
+//         <img src="${flags.png}">
+//     </div>
+//  `
+// }
+
+
+// original
+// const getCountryHtml = country=>{
+//     return `
+//        <div class ='country'>
+//            <h1>${country.name.common}</h1>
+//            <img src="${country.flags.png}">
+//        </div>
+//     `
+//    }
 loadCountries();
