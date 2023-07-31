814c7be78d48d8568cea3af2549e7010f1fd4546
SoumenBaral <shumen.sb@gmail.com>
2023-07-31 10:26:29 +0600

    first commit

---
+++ 0.soumen/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import logo from './logo.svg'
+function App() {
+  return (
+    <div className="App">
+     <h1>Hi I am from react</h1>
+    </div>
+  );
+}
+
+export default App;
