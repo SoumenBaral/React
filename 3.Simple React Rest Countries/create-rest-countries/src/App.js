5988cc7f1ee1de8aa7997bbf8b8869757ad18a69
SoumenBaral <shumen.sb@gmail.com>
2023-08-01 02:40:29 +0600

    create parant and child component

--- 3.Simple React Rest Countries/create-rest-countries/src/App.js
+++ 3.Simple React Rest Countries/create-rest-countries/src/App.js
@@ -1,31 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
-import Country from './components/Country/Country';
 import Header from './components/Header/Header';
+import Countries from './components/Countries/Countries';
 function App() {
   return (
     <div className="App">
       <Header></Header>
-     <LoadCountries></LoadCountries>
+      <Countries></Countries>
     </div>
   );
 }
-function LoadCountries (){
-  const[Countries,setCountries] = useState([]);
-  useEffect(()=>{
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res=>res.json())
-    .then(data=>setCountries(data))
-  },[])
-  return(
-    <div>
-      <h1>Visiting Every Country in the World</h1>
-      <p>All Countries : {Countries.length}</p>
-      {
-        Countries.map(country=><Country name={country.name.common} key={country.cca3} population ={country.population}></Country>)
-      }
-    </div>
-  )
-}
+
 export default App;
---
+++ 3.Simple React Rest Countries/create-rest-countries/src/components/Countries/Countries.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import Country from '../Country/Country';
+
+const Countries = () => {
+    const[Countries,setCountries] = useState([]);
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res=>res.json())
+    .then(data=>setCountries(data))
+  },[])
+  return(
+    <div>
+      <h1>Visiting Every Country in the World</h1>
+      <p>All Countries : {Countries.length}</p>
+      {
+        Countries.map(country=><Country name={country.name.common} key={country.cca3} population ={country.population}></Country>)
+      }
+    </div>
+  )
+};
+
+export default Countries;
