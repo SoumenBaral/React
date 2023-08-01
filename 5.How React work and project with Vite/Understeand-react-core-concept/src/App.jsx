2835d7665074712f9eeaaac4ccc59f85318f1b40
SoumenBaral <shumen.sb@gmail.com>
2023-08-01 20:57:26 +0600

    create a vite project

---
+++ 5.How React work and project with Vite/Understeand-react-core-concept/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Device from './components/Device/Device'
+
+function App() {
+  
+
+  return (
+    <div className='card'>
+     <Device name={'Samsung'} price={100000}></Device>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ 5.How React work and project with Vite/Understeand-react-core-concept/src/components/Device/Device.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Device = ({name}) => {
+    return (
+        <div>
+            <h3>I have a Device: {name}</h3>
+        </div>
+    );
+};
+
+export default Device;
