52f97211a667d94499ccbde596bac94dac052138
SoumenBaral <shumen.sb@gmail.com>
2023-08-02 14:08:16 +0600

    pass a event hendeler vai arrow function

--- 6.Modules and data storage intrigration/react-storage-and-handeler/src/Components/Cosmetic/Cosmetic.js
+++ 6.Modules and data storage intrigration/react-storage-and-handeler/src/Components/Cosmetic/Cosmetic.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import './Cosmetic.css'
 const Cosmetic = ({cosmetic}) => {
-const {name,price} = cosmetic;
+const {name,price,id} = cosmetic;
+const AddToCart=(id)=>{
+    console.log(id);
+}
     return (
-        <div>
+        <div className='product'>
             <h3>Buy This : {name}</h3>
             <p>Only for :  {price}</p>
+            <button onClick={()=>AddToCart(id)}>Add to Cart </button>
         </div>
     );
 };
