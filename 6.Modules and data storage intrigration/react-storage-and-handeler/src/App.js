87a2b808927ff6c5a333df0ca86e14481632e6bf
SoumenBaral <shumen.sb@gmail.com>
2023-08-02 13:15:14 +0600

    learn component

---
+++ 6.Modules and data storage intrigration/react-storage-and-handeler/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Cosmetics from './Components/Cosmetics/Cosmetics';
+
+function App() {
+  return (
+    <div className="App">
+    <Cosmetics></Cosmetics> 
+    </div>
+  );
+}
+
+export default App;
---
+++ 6.Modules and data storage intrigration/react-storage-and-handeler/src/Components/Cosmetics/Cosmetics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cosmetics = () => {
+    return (
+        <div>
+            <h1>Welcome to my Cosmetic Store </h1>
+        </div>
+    );
+};
+
+export default Cosmetics;
