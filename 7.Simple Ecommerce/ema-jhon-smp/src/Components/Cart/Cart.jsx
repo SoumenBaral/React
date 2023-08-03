a1a8f82efcff531c2639fa88f700dc1cfb0eb3db
SoumenBaral <shumen.sb@gmail.com>
2023-08-03 20:29:21 +0600

    all done

--- 7.Simple Ecommerce/ema-jhon-smp/src/Components/Cart/Cart.jsx
+++ 7.Simple Ecommerce/ema-jhon-smp/src/Components/Cart/Cart.jsx
@@ -3,16 +3,23 @@ import './Cart.css'
 const Cart = ({carts}) => {
     let price =0;
     let TotalShipping = 0;
+    let Quantity = 0;
     for(let cart of carts){
-        price+=parseInt(cart.price);
-        TotalShipping +=cart.shipping;
+        if(cart.quantity===0)
+        {
+            cart.quantity =1;
+        }
+        // cart.quantity =cart.quantity||1;
+        price+=parseInt(cart.price)*cart.quantity;
+        TotalShipping +=cart.shipping*cart.quantity;
+        Quantity +=cart.quantity;
     }
     const totalTax =price*7/100;
     const allTotal = price+TotalShipping+totalTax;
     return (
         <div className='carts'>
             <h4 className='Order'>Order Summary</h4>
-            <p>selected Item: {carts.length}</p>
+            <p>selected Item: {Quantity}</p>
             <p>Total Price: ${price}</p>
             <p>Shipping: ${TotalShipping}</p>
             <p>Tax: ${totalTax.toFixed(2)}</p>
