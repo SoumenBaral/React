dd5b5a702b3258c78b5325cff534c263f18b211d
SoumenBaral <shumen.sb@gmail.com>
2023-08-05 02:21:12 +0600

    add TailWind in project

---
+++ 8.Practice/player-bazer/src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='bg-red-500'>Welcome to my website..</h1>
+        </div>
+    );
+};
+
+export default Header;
