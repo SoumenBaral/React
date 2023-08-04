0ff8d5f92f4daf97d48b7fc0e0753f094c1752af
SoumenBaral <shumen.sb@gmail.com>
2023-08-05 03:33:14 +0600

    hero section

--- 8.Practice/player-bazer/src/App.js
+++ 8.Practice/player-bazer/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header/Header';
+import HeroSection from './Components/HeroSection/HeroSection';
 
 function App() {
   return (
     <div className="App">
      <Header></Header>
+     <HeroSection></HeroSection>
     </div>
   );
 }
--- 8.Practice/player-bazer/src/Components/HeroSection/HeroSection.jsx
+++ 8.Practice/player-bazer/src/Components/HeroSection/HeroSection.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const HeroSection = () => {
+    const images = [
+        'https://i.ibb.co/Js78PRF/Hero-Section.jpg',
+        'https://i.ibb.co/BZtsRdg/Hero2.webp',
+        'https://i.ibb.co/4gGfcgR/hero5.jpg',
+        'https://i.ibb.co/y4dYXSg/10-cristiano-ronaldo-football-hd-wallpapers.jpg',
+        'https://i.ibb.co/dBKYthX/football-players-expression-hd-football.jpg',
+        'https://i.ibb.co/7KNM2KM/soccer-player-man-human-person-sport.jpg',
+        'https://i.ibb.co/88kq2pQ/soccer-lionel-messi-fc-barcelona-wallpaper-preview.jpg'
+    ];
+
+    const [imgIndex, setImgIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setImgIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 1000);
+
+        // Clean up the interval when the component unmounts
+        return () => clearInterval(interval);
+    }, [images.length]);
+
     return (
         <div>
-            
+            <section className="slider">
+                <img
+                    className='w-full'
+                    id="image_slider"
+                    src={images[imgIndex]}
+                    alt={`Slide ${imgIndex}`}
+                />
+            </section>
         </div>
     );
 };
