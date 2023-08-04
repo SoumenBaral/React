192deac97d554656ebef30f2f35cf604110a43cf
SoumenBaral <shumen.sb@gmail.com>
2023-08-05 03:45:32 +0600

    add player component

--- 8.Practice/player-bazer/src/Components/PlayerShop/PlayerShop.jsx
+++ 8.Practice/player-bazer/src/Components/PlayerShop/PlayerShop.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const PlayerShop = () => {
     return (
         <div>
-            
+            <div className="player-Container">
+                <div>
+
+                </div>
+                <div>
+                    
+                </div>
+            </div>
         </div>
     );
 };
