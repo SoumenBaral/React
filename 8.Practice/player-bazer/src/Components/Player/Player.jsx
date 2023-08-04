87ae98ca33681484a5da583e12e1bcc611523a1a
SoumenBaral <shumen.sb@gmail.com>
2023-08-05 04:44:44 +0600

    all done for night

--- 8.Practice/player-bazer/src/Components/Player/Player.jsx
+++ 8.Practice/player-bazer/src/Components/Player/Player.jsx
@@ -6,6 +6,7 @@ const Player = () => {
             <h1>Players And and all</h1>
             <h2>ok</h2>
             <h3>5 Days Done wow....</h3>
+            <p>Good night </p>
         </div>
     );
 };
