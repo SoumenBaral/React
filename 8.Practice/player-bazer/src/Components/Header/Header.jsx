89a37e77886f1659482aebf9fe2c47833c6de601
SoumenBaral <shumen.sb@gmail.com>
2023-08-05 03:01:03 +0600

    header done

--- 8.Practice/player-bazer/src/Components/Header/Header.jsx
+++ 8.Practice/player-bazer/src/Components/Header/Header.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            <h1 className='bg-red-500'>Welcome to my website..</h1>
+        <div className='bg-black text-white md:flex justify-between  items-center'>
+            <div className='flex justify-center items-center mx-8'>
+            <img className='w-[100px]' src="https://i.ibb.co/ZTQ3jR6/logo.jpg" alt="logo" />
+            <h3 className='text-3xl text-orange-500'>Player-Bazar</h3>
+            </div>
+            <div>
+                <ul className='text-lg flex justify-center items-center gap-4 mx-10 cursor-pointer'>
+                    <li className=' hover:text-blue-800  font-semibold'>Home</li>
+                    <li className=' hover:text-blue-800  font-semibold'>About</li>
+                    <li className=' hover:text-blue-800  font-semibold'>Manager</li>
+                    <li className=' hover:text-blue-800  font-semibold'>Blog</li>
+                    <li className=' hover:text-blue-800  font-semibold'>Login</li>
+
+                </ul>
+            </div>
         </div>
     );
 };
